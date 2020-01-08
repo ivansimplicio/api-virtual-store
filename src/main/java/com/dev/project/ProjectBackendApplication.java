9f068c406bb5fa2a366929ecf831490727b09b92
ivansimplicio <ivansimplicio@outlook.com>
2020-01-08 17:11:59 -0300

    entidades Estado e Cidade criadas

--- src/main/java/com/dev/project/ProjectBackendApplication.java
+++ src/main/java/com/dev/project/ProjectBackendApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.dev.project.domain.Categoria;
+import com.dev.project.domain.Cidade;
+import com.dev.project.domain.Estado;
 import com.dev.project.domain.Produto;
 import com.dev.project.repositories.CategoriaRepository;
+import com.dev.project.repositories.CidadeRepository;
+import com.dev.project.repositories.EstadoRepository;
 import com.dev.project.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,6 +23,10 @@ public class ProjectBackendApplication implements CommandLineRunner {
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectBackendApplication.class, args);
@@ -43,5 +51,18 @@ public class ProjectBackendApplication implements CommandLineRunner {
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 	}
 }
