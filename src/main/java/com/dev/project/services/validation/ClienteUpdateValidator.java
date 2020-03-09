26a0229d6b14ff001cfbb175dde92efbba22b71b
ivansimplicio <ivansimplicio@outlook.com>
2020-03-09 20:07:48 -0300

    validação customizada: email não repetido na atualização de Cliente

---
+++ src/main/java/com/dev/project/services/validation/ClienteUpdateValidator.java
@@ -0,0 +1,53 @@
+package com.dev.project.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import com.dev.project.domain.Cliente;
+import com.dev.project.dto.ClienteDTO;
+import com.dev.project.repositories.ClienteRepository;
+import com.dev.project.resources.exception.FieldMessage;
+
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+	
+	@Autowired
+	private HttpServletRequest request;
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	@Override
+	public void initialize(ClienteUpdate ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteDTO objDTO, ConstraintValidatorContext context) {
+		
+		@SuppressWarnings("unchecked")
+		Map<String, String> map = (Map<String, String>)request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Integer uriId = Integer.parseInt(map.get("id"));
+		
+		List<FieldMessage> list = new ArrayList<>();
+		
+		Cliente aux = repo.findByEmail(objDTO.getEmail());
+		
+		if(aux != null && !aux.getId().equals(uriId)) {
+			list.add(new FieldMessage("email", "O email informado já está cadastrado."));
+		}
+
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
