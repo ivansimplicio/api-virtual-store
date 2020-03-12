1101fab2719cd85400e18f4a0a9e33ef41d7119c
ivansimplicio <ivansimplicio@outlook.com>
2020-03-12 14:37:00 -0300

    busca de produtos por nome e categorias

---
+++ src/main/java/com/dev/project/dto/ProdutoDTO.java
@@ -0,0 +1,46 @@
+package com.dev.project.dto;
+
+import java.io.Serializable;
+
+import com.dev.project.domain.Produto;
+
+public class ProdutoDTO implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String nome;
+	private Double preco;
+	
+	public ProdutoDTO() {
+	}
+	
+	public ProdutoDTO(Produto produto) {
+		setId(produto.getId());
+		setNome(produto.getNome());
+		setPreco(produto.getPreco());
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+}
