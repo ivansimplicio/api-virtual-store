47854fa421766aabf2017f56e54f4ba7fd951479
ivansimplicio <ivansimplicio@outlook.com>
2020-03-12 15:23:49 -0300

    cálculo do subtotal e total do Pedido

--- src/main/java/com/dev/project/domain/Pedido.java
+++ src/main/java/com/dev/project/domain/Pedido.java
@@ -53,6 +53,14 @@ public class Pedido implements Serializable{
 		this.cliente = cliente;
 		this.enderecoDeEntrega = enderecoDeEntrega;
 	}
+	
+	public double getValorTotal() {
+		double soma = 0;
+		for(ItemPedido i : itens) {
+			soma += i.getSubTotal();
+		}
+		return soma;
+	}
 
 	public Integer getId() {
 		return id;
