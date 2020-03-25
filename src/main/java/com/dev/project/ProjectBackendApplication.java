371288a5e17382cb1fc4824b69fae20ca24c2e2d
ivansimplicio <ivansimplicio@outlook.com>
2020-03-25 09:41:27 -0300

    enviando imagem via endpoint

--- src/main/java/com/dev/project/ProjectBackendApplication.java
+++ src/main/java/com/dev/project/ProjectBackendApplication.java
@@ -1,18 +1,11 @@
 package com.dev.project;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.dev.project.services.S3Service;
-
-
 @SpringBootApplication
 public class ProjectBackendApplication implements CommandLineRunner {
-	
-	@Autowired
-	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectBackendApplication.class, args);
@@ -20,6 +13,5 @@ public class ProjectBackendApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		s3Service.uploadFile("C:\\fotos\\image1.jpg");
 	}
 }
--- src/main/java/com/dev/project/resources/ClienteResource.java
+++ src/main/java/com/dev/project/resources/ClienteResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.dev.project.domain.Cliente;
@@ -81,5 +82,11 @@ public class ClienteResource {
 		Page<Cliente> list = service.findPage(page, linesPerPage, orderBy, direction);
 		Page<ClienteDTO> listDTO = list.map(obj -> new ClienteDTO(obj));
 		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	@RequestMapping(value="/picture", method=RequestMethod.POST)
+	public ResponseEntity<Void> uploadProfilePicture(@RequestParam(name="file") MultipartFile multipartFile){
+		URI uri = service.uploadProfilePicture(multipartFile);	
+		return ResponseEntity.created(uri).build();
 	}
 }
